3d0852e584bb4d71c792ed8d44ceb5b069dcd6b8
fikserr <azizxon7777777@gmail.com>
2024-02-09 11:17:58 +0500

    load

---
+++ jss/app.js
@@ -0,0 +1,13 @@
+const imgContent = document.querySelector('.img__content');
+const image = document.querySelectorAll('img.image');
+const body = document.querySelector('body');
+
+imgContent.addEventListener('click', function(e){
+    let javob = e.target
+    javob.requestFullscreen();
+    
+});
+
+document.addEventListener('dblclick', function(){
+    document.exitFullscreen();
+});
